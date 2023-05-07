c90ee5b3086feb4fd4e9bffad53a466b3be30229
dayeon <chuen1234@naver.com>
2023-05-07 13:31:19 +0900

    23/05/07 일요일

--- tech-term-prj03/src/component/Day.js
+++ tech-term-prj03/src/component/Day.js
@@ -1,12 +1,24 @@
 import dummy from "../db/data.json"
 import {useParams} from "react-router-dom"
 import Word from './Word'
+import {useEffect, useState} from 'react'
 
 
 export default function Day(){
 
     // const inputDay = 3;
     const {inputDay} = useParams();
+    // const {words, setWords} = useState([]);
+
+    // useEffect( () => {
+    //     // fetch("https://localhost:4000/words?day=1")
+    //     fetch(`https://localhost:4000/words?day=${inputDay}`)
+    //     .then(res => {return res.json();})
+    //     .then(data => {setWords(data);});
+    // },    []
+    // )
+
+    const words = useFetch('https://localhost:4000/words?day=${inputDay}`')
 
     const wordList = dummy.words.filter( word=> (word.day===Number(inputDay)))
     // wordList : filter함수로 day가 3인 요소들을 담은 배열집합 (words)
